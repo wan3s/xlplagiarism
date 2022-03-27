2c356705c8fc4c25bdcc3015315a026c15bf9be5
Ivan <iburimskiy@gmail.com>
2022-03-27 17:50:08 +0300

    Push unpushed

---
+++ scripts/detectors/common.py
@@ -0,0 +1,3 @@
+class BaseDetector:
+    def count_similiarity(self, src_lang_text, dst_lang_text):
+        raise NotImplementedError
---
+++ scripts/main.py
@@ -0,0 +1,24 @@
+import pathlib
+
+from detectors.labse import definition as labse_definition
+from prepare_texts import common
+
+SHUFFLED_TEXTS = pathlib.Path('../shuffled')
+
+def main():
+    labse_detector = labse_definition.LabseDetector()
+    for src_lang_file_path in common.TRANSLATED_TEXTS.glob(f'{common.SRC_LANG}/*'):
+        with open(src_lang_file_path) as inp_file:
+            src_lang_text = inp_file.read()
+        filename = src_lang_file_path.stem
+        dst_lang_file_path = common.TRANSLATED_TEXTS.joinpath(
+            f'{common.DST_LANG}/{filename}'
+        )
+        with open(dst_lang_file_path) as inp_file:
+            dst_lang_text = inp_file.read()
+        print(
+            f'{filename}: {labse_detector.count_similiarity(src_lang_text, dst_lang_text)}'
+        )
+
+if __name__ == '__main__':
+    main()
