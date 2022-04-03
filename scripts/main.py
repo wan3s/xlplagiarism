fbe247e5dc9594524243052a65938c88bab15c1b
Ivan <iburimskiy@gmail.com>
2022-04-03 12:17:16 +0300

    Tfidf

--- scripts/main.py
+++ scripts/main.py
@@ -1,11 +1,12 @@
 import pathlib
 
 from detectors.labse import definition as labse_definition
+from detectors.tfidf import definition as tfifd_definition
 from prepare_texts import common
 
 SHUFFLED_TEXTS = pathlib.Path('../shuffled')
 
-def main():
+def compare():
     labse_detector = labse_definition.LabseDetector()
     for src_lang_file_path in common.TRANSLATED_TEXTS.glob(f'{common.SRC_LANG}/*'):
         with open(src_lang_file_path) as inp_file:
@@ -19,6 +20,12 @@ def main():
         print(
             f'{filename}: {labse_detector.count_similiarity(src_lang_text, dst_lang_text)}'
         )
+
+
+def main():
+    print('Started ...')
+    print(tfifd_definition.count_idf())
+
 
 if __name__ == '__main__':
     main()
