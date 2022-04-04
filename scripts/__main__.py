c3addd72bba13e8ac56033526b5f980c1df5a66e
Ivan <iburimskiy@gmail.com>
2022-04-04 16:45:27 +0300

    Improvements

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -11,7 +11,7 @@ from scripts.prepare_texts import shuffle_texts as shuffle_prepared_texts
 from progress import bar
 
 MAX_DST_FILES = 10
-DIFFERENT_FILES_NUM = 100
+DIFFERENT_FILES_NUM = 10
 
 def run_experiments(args):
     labse_detector = labse_definition.LabseDetector()
@@ -20,9 +20,10 @@ def run_experiments(args):
     with open(consts.SHUFFLED_TEXTS.joinpath('originality')) as inp_file:
         originality = eval(inp_file.read())
     print('Preparing completed!')
-    src_texts_paths = [path for path in consts.SHUFFLED_TEXTS.glob(f'{consts.SRC_LANG}/*')][:10]
-    # progress_bar = bar.IncrementalBar('Comparison', max=len(src_texts_paths))
+    src_texts_paths = [path for path in consts.SHUFFLED_TEXTS.glob(f'{consts.SRC_LANG}/*')][:100]
+    #progress_bar = bar.IncrementalBar('Comparison', max=len(src_texts_paths) * MAX_DST_FILES)
     different_files_cntr = 0
+    res_output = []
     for src_lang_file_path in src_texts_paths:
         src_file_name = src_lang_file_path.stem
         with open(src_lang_file_path) as inp_file:
@@ -48,11 +49,15 @@ def run_experiments(args):
             acc, labse_sim, tfidf_sim, shingles_sim = [
                 str(x).replace('.', ',') for x in [acc, labse_sim, tfidf_sim, shingles_sim]
             ]
-            print(f'{src_file_name}:{dst_file_name};{acc};{labse_sim};{tfidf_sim};{shingles_sim}')
-            #print(f'{src_file_name}:{dst_file_name},{acc},{shingles_sim}')
+            res = f'{src_file_name}:{dst_file_name};{acc};{labse_sim};{tfidf_sim};{shingles_sim}'
+            #res = f'{src_file_name}:{dst_file_name};{acc}'
+            res_output.append(res)
+            print(res)
             comparisons_num += 1
-            # progress_bar.next()
-    # progress_bar.finish()
+            #progress_bar.next()
+    #progress_bar.finish()
+    with open('result.csv', 'w') as out_file:
+        out_file.write('\n'.join(res_output))
 
 
 def prepare_texts(args):
