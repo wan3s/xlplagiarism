31cc684d632e4ffee6c1f270b227104e2cf77201
iburimskii <iburimskii@yandex-team.ru>
2022-05-10 10:22:24 +0300

    Improvements

--- scripts/experiments/compare_on_public_datasets.py
+++ scripts/experiments/compare_on_public_datasets.py
@@ -13,16 +13,16 @@ from scripts.detectors.labse import definition as labse_definition
 def run(args):
     labse_detector = labse_definition.LabseDetector(sim_threshold=args.labse_sim_threshold)
     masks_and_data = [
+        ('chunks', consts.CHUNK_MASKS, consts.CONF_PAPERS_CHUNKS_EN, consts.CONF_PAPERS_CHUNKS_FR),
         ('documents', consts.DOCS_MASKS, consts.CONF_PAPERS_DOCS_EN, consts.CONF_PAPERS_DOCS_FR),
         ('sentences', consts.SENTENCE_MASKS, consts.CONF_PAPERS_SENTENCES_EN, consts.CONF_PAPERS_SENTENCES_FR),
-        ('chunks', consts.CHUNK_MASKS, consts.CONF_PAPERS_CHUNKS_EN, consts.CONF_PAPERS_CHUNKS_FR),
     ]
     for level, masks_dir, en_dir, fr_dir in masks_and_data:
         total_tn_fp_fn_tp = [0, 0, 0, 0]
         for masks in [x for x in masks_dir.glob('*')][:1]:
             tn_fp_fn_tp = [0, 0, 0, 0]
             with open(masks, 'r') as mask_file:
-                mask_file_lines = [x for x in mask_file.read().split('\n') if x][:10000]
+                mask_file_lines = [x for x in mask_file.read().split('\n') if x]
             contents_by_filenames = {}
             loaded_masks = []
             progress_bar_content_coll = bar.IncrementalBar(f'Collecting content on {level}-level', max=len(mask_file_lines))
@@ -38,9 +38,10 @@ def run(args):
                     with open(en_dir.joinpath(en_text_file), 'r') as en_inp_file:
                         contents_by_filenames[en_text_file] = en_inp_file.read()
                 except FileNotFoundError:
-                    # print('File not found => continue')
+                    print('File not found => continue')
                     pass
-                
+                if fr_file_template != en_file_template:
+                    continue
                 loaded_masks.append(
                     {
                         'equal': int(fr_file_template == en_file_template),
@@ -58,19 +59,24 @@ def run(args):
                 for mask in loaded_masks:
                     fr_text_file, en_text_file = mask['fr'], mask['en']
                     progress_bar_comp.next()
-                    def _worker(fr_text_file, en_text_file):
-                        fr_text, en_text = contents_by_filenames[fr_text_file], contents_by_filenames[en_text_file]
+                    def _worker(mask_worker):
+                        fr_text_file_w, en_text_file_w = mask_worker['fr'], mask_worker['en']
+                        equal = mask_worker['equal']
+                        fr_text, en_text = contents_by_filenames[fr_text_file_w], contents_by_filenames[en_text_file_w]
                         y = int(labse_detector.count_similiarity_whole_text(fr_text, en_text) > labse_detector.sim_threshold)
-                        idx = y + mask['equal'] * 2
-                        return idx
-                    futures.append(executor.submit(_worker, fr_text_file, en_text_file))
+                        return y + equal * 2
+                    futures.append(executor.submit(_worker, mask))
                 progress_bar_comp.finish()
                 progress_bar_res = bar.IncrementalBar(f'Result on {level}-level', max=len(loaded_masks))
+                print_idx = 0
                 for future in concurrent.futures.as_completed(futures):
                     progress_bar_res.next()
                     idx = future.result()
                     tn_fp_fn_tp[idx] += 1
                     total_tn_fp_fn_tp[idx] += 1
+                    print_idx += 1
+                    if not print_idx % 500:
+                        print(f'Intermediate result tn_fp_fn_tp: {tn_fp_fn_tp}')
                 progress_bar_res.finish()
             print(f'\ntn_fp_fn_tp: {tn_fp_fn_tp}')
             print(time.time() - without_threads_start)
